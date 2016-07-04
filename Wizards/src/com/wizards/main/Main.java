ce4bca8f9121f17955138fbd419cbb21264b0f7c
Tyler <t.eldred4646@gmail.com>
2016-07-03 23:43:11 -0600

    Wizards game Start Screen
    This is just the tip of the iceberg for the start screen.

--- Wizards/src/com/wizards/main/Main.java
+++ Wizards/src/com/wizards/main/Main.java
@@ -1,14 +1,116 @@
 package com.wizards.main;
 
-public class Main {
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
 
-	public Main() {
-		
-		
+import javax.imageio.*;
+import javax.swing.*;
+
+public class Main extends JComponent implements MouseListener, MouseMotionListener{
+	
+	//VARIABLES
+	public int WIDTH = 800;
+	public int HEIGHT = 600;
+	
+	public boolean showTitleScreen = true;
+	public boolean showBuildScreen = false;
+	public boolean showPlayScreen = false;
+	
+	BufferedImage btnBeginDef;
+	BufferedImage btnBeginPress;
+	BufferedImage btnBeginHov;
+	BufferedImage btnBeginState;
+	
+	
+	//CONSTRUCTOR
+	public Main() throws IOException {
+		btnBeginDef = ImageIO.read(getClass().getResource("ButtonBeginDefault.png"));
+		btnBeginHov = ImageIO.read(getClass().getResource("ButtonBeginHovered.png"));
+		btnBeginPress = ImageIO.read(getClass().getResource("ButtonBeginPressed.png"));
+		btnBeginState = btnBeginDef;
 	}
 	
-	public static void main(String[] args) {
+	
+	//MAIN METHOD
+	public static void main(String[] args) throws IOException {
+		
+		JFrame window = new JFrame("Wizards");
+		Main game = new Main();
+		window.add(game);
+		window.pack();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLocationRelativeTo(null);
+		window.setResizable(false);
+		window.setVisible(true);
+		
+		game.addMouseListener(game);
+		game.addMouseMotionListener(game);
 		
 	}
+	public Dimension getPreferredSize() {
+		return new Dimension(WIDTH, HEIGHT);
+	}
+	
+	
+	//GRAPHICS
+	protected void paintComponent(Graphics g) {
+		
+		//TITLE SCREEN
+		if (showTitleScreen) {
+			//Background
+			g.setColor(new Color(244, 231, 129));
+			g.fillRect(0, 0, WIDTH, HEIGHT);
+			
+			g.setColor(Color.BLACK);
+			g.fillRect(50, 50, 700, 150);
+			
+			//Button
+			g.drawImage(btnBeginState, (WIDTH - 150) / 2, 225, null);
+		}
+		
+		//PLAY SCREEN
+		if (showPlayScreen) {
+			g.setColor(Color.green);
+			g.fillRect(0, 0, WIDTH, HEIGHT);
+		}
+		
+	}
+
+	
+	//MouseListeners
+	public void mousePressed(MouseEvent e) {
+		if (showTitleScreen) {
+			btnBeginState = btnBeginPress;
+		}
+		
+		repaint();
+	}
+	public void mouseReleased(MouseEvent e) {
+		if (showTitleScreen) {
+			if (e.getX() >= (WIDTH - 150) / 2 && e.getX() <= (WIDTH - 150) / 2 + 150 && e.getY() >= 225 && e.getY() <= 225 + 50) {
+				showTitleScreen = false;
+				showPlayScreen = true;
+			}
+			btnBeginState = btnBeginDef;
+		}
+		
+		repaint();
+	}
+	public void mouseMoved(MouseEvent e) {
+		if (showTitleScreen) {
+			if (e.getX() >= (WIDTH - 150) / 2 && e.getX() <= (WIDTH - 150) / 2 + 150 && e.getY() >= 225 && e.getY() <= 225 + 50)
+				btnBeginState = btnBeginHov;
+			else
+				btnBeginState = btnBeginDef;
+		}
+		
+		repaint();
+	}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseDragged(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {}
 	
 }
