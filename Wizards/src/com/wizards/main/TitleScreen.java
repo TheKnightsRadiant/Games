c2dc234c563c5d4fdfa0bad5eec5fd3d5ef61a5c
ThatDude11 <ian12343456@gmail.com>
2016-07-04 12:43:30 -0600

    Thing

--- Wizards/src/com/wizards/main/TitleScreen.java
+++ Wizards/src/com/wizards/main/TitleScreen.java
@@ -1,5 +1,13 @@
 package com.wizards.main;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class TitleScreen {
 
+	public void paint(Graphics g) {
+		g.setColor(Color.blue);
+		g.fillRect(0, 0, 800, 600);
+	}
+	
 }
