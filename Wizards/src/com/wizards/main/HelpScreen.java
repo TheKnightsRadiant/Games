2d3c0122cf7e2265d95fd69f4aa4a04a658d0e02
Tyler <t.eldred4646@gmail.com>
2016-07-04 22:16:19 -0600

    fixed HelpClass

--- Wizards/src/com/wizards/main/HelpScreen.java
+++ Wizards/src/com/wizards/main/HelpScreen.java
@@ -3,14 +3,6 @@ package com.wizards.main;
 import java.awt.*;
 
 public class HelpScreen {
-<<<<<<< HEAD
-	
-	void drawString(Graphics g, String text, int x, int y) {
-	    for (String line : text.split("\n"))
-	        g.drawString(line, x, y += g.getFontMetrics().getHeight());
-	}
-	
-=======
 
 	private int pageNumber = 2;
 
@@ -42,34 +34,28 @@ public class HelpScreen {
 		}
 	}
 
->>>>>>> master
 	public void paint(Graphics g) {
 		// BACKGROUND
 		g.setColor(new Color(244, 231, 129));
-<<<<<<< HEAD
 		g.fillRect(0, 0, 800, 600);
 		
 		g.setColor(new Color(255, 250, 150));
 		g.fillRect(25, 100, 750, 475);
 		
 		//HELP TITLE
-=======
 		g.fillRect(0, 0, 810, 610);
 
 		g.setColor(new Color(255, 250, 150));
 		g.fillRect(25, 100, 760, 485);
 
 		// HELP TITLE
->>>>>>> master
 		g.setColor(new Color(192, 143, 38));
 		g.setFont(new Font("ZapfDingbats", Font.BOLD, 50));
 		g.drawString("Help", 340, 75);
 		
-<<<<<<< HEAD
 		//HELP CONTENTS
 		g.setFont(new Font("ZapfDingbats", Font.BOLD, 15));
 		this.drawString(g, "This is the help page.\nMatt, please tell me what you want me to write here...\n\n--Tyler", 50, 125);
-=======
 		g.setFont(new Font("ZapfDingbats", Font.BOLD, 12));
 		g.drawString("Page: " + pageNumber, 600, 75);
 
@@ -99,7 +85,6 @@ public class HelpScreen {
 		default:
 			System.err.println("There was an error changing the pages.");
 		}
->>>>>>> master
 	}
 
 }
