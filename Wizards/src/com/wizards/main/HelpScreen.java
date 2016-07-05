79d977d2766e0d608ccf1fa5b8f18bcea82cef3f
Tyler <t.eldred4646@gmail.com>
2016-07-04 22:19:12 -0600

    Fixed next/prev buttons

--- Wizards/src/com/wizards/main/HelpScreen.java
+++ Wizards/src/com/wizards/main/HelpScreen.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class HelpScreen {
 
-	private int pageNumber = 2;
+	private int pageNumber = 1;
 
 	void drawString(Graphics g, String text, int x, int y) {
 		for (String line : text.split("\n"))
@@ -53,11 +53,9 @@ public class HelpScreen {
 		g.setFont(new Font("ZapfDingbats", Font.BOLD, 50));
 		g.drawString("Help", 340, 75);
 		
-		//HELP CONTENTS
-		g.setFont(new Font("ZapfDingbats", Font.BOLD, 15));
-		this.drawString(g, "This is the help page.\nMatt, please tell me what you want me to write here...\n\n--Tyler", 50, 125);
 		g.setFont(new Font("ZapfDingbats", Font.BOLD, 12));
 		g.drawString("Page: " + pageNumber, 600, 75);
+
 
 		// HELP CONTENTS
 		g.setFont(new Font("ZapfDingbats", Font.BOLD, 18));
