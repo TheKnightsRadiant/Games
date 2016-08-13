cdf988b6389e601e543d7a12f37a3deb3851c169
MatthewMorse <wheeloftimerocks@gmail.com>
2016-08-13 15:01:05 -0600

    Creating Horde Mode

--- Wizards/src/com/terracore/wizards/PlayScreenHorde.java
+++ Wizards/src/com/terracore/wizards/PlayScreenHorde.java
@@ -1,7 +1,9 @@
 package com.terracore.wizards;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import com.terracore.storages.Textures;
 
@@ -11,13 +13,17 @@ public class PlayScreenHorde {
 		g.setColor(new Color(244, 231, 129));
 		g.fillRect(0, 0, Wizards.WIDTH , Wizards.HEIGHT);
 		
-		Textures.blankCreatureCard.render(g, Wizards.compareToWidth(220), Wizards.compareToHeight(20));
-		Textures.blankSpellCard.render(g, Wizards.compareToWidth(120), Wizards.compareToHeight(20));
-		Textures.blankCard.render(g, Wizards.compareToWidth(20), Wizards.compareToHeight(20));
+		Textures.blankCreatureCard.render(g, Wizards.compareToWidth(580), Wizards.compareToHeight(320));
+		Textures.blankSpellCard.render(g, Wizards.compareToWidth(480), Wizards.compareToHeight(320));
+		Textures.blankCard.render(g, Wizards.compareToWidth(380), Wizards.compareToHeight(320));
+	
+		g.setColor(new Color(28, 28, 22));
+		g.drawLine(0, 300, 800, 300);
+		//Horizontal Deck Line
+		g.drawLine(200, 0, 200, 600);
 		
-		Textures.blankCard.render(g, Wizards.compareToWidth(20), Wizards.compareToHeight(320));
-		Textures.blankSpellCard.render(g, Wizards.compareToWidth(170), Wizards.compareToHeight(320));
-		Textures.blankCreatureCard.render(g, Wizards.compareToWidth(320), Wizards.compareToHeight(320));
+		g.setFont(new Font("ZapfDingbats", Font.BOLD, 31));
+		
+		g.drawString("Your Hand:", Wizards.compareToWidth(210), Wizards.compareToHeight(345));
 	}
-
 }
