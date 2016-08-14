2bf17c275ed546b614c5aac66d4c36c3c2e4341c
ThatDude11 <ian12343456@gmail.com>
2016-08-13 21:39:17 -0600

    Randomization is better

--- Wizards/src/com/terracore/storages/Cards.java
+++ Wizards/src/com/terracore/storages/Cards.java
@@ -6,18 +6,26 @@ import com.terracore.cards.CreatureCard;
 
 public class Cards {
 
-	private static int CardNum = 1;
+	private static int CardNum = 3;
 
 	static Random r = new Random();
 
-	static CreatureCard Firetruck = new CreatureCard(0, 100, 10, 10, 10, 10, 9, 7, "Water", "Firetruck",
+	static CreatureCard BlankCreature = new CreatureCard(0, 0, 0, 0, 0, 0, 0, 0, "null", "blank",
+			Textures.blankCreatureCard);
+	static CreatureCard BlankSpell = new CreatureCard(1, 0, 0, 0, 0, 0, 0, 0, "null", "blank",
+			Textures.blankSpellCard);
+	static CreatureCard Firetruck = new CreatureCard(2, 100, 10, 10, 10, 10, 9, 7, "Water", "Firetruck",
 			Textures.creatureCardFireTruck);
+	
 
 	public static CreatureCard getCard() {
 		switch (r.nextInt(CardNum)) {
 		case (0):
+			return BlankCreature;
+		case (1):
+			return BlankSpell;
+		case (2):
 			return Firetruck;
-		//case (1):
 		}
 		return null;
 	}
