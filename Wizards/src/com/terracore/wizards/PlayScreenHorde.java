c1cf50a21d34774164516e46c9115a895ff1bd15
MatthewMorse <wheeloftimerocks@gmail.com>
2016-08-13 21:15:09 -0600

    Horde

--- Wizards/src/com/terracore/wizards/PlayScreenHorde.java
+++ Wizards/src/com/terracore/wizards/PlayScreenHorde.java
@@ -17,29 +17,31 @@ public class PlayScreenHorde {
 		g.setColor(new Color(244, 231, 129));
 		g.fillRect(0, 0, Wizards.WIDTH , Wizards.HEIGHT);
 		
-		Textures.blankCreatureCard.render(g, Wizards.compareToWidth(580), Wizards.compareToHeight(320));
-		Textures.spellCardFireball.render(g, Wizards.compareToWidth(480), Wizards.compareToHeight(320));
+		Textures.blankCreatureCard.render(g, Wizards.compareToWidth(640), Wizards.compareToHeight(320));
+		Textures.spellCardFireball.render(g, Wizards.compareToWidth(485), Wizards.compareToHeight(320));
+		Textures.blankCreatureCard.render(g, Wizards.compareToWidth(330), Wizards.compareToHeight(320));
+		Textures.creatureCardFireTruck.render(g, Wizards.compareToWidth(175), Wizards.compareToHeight(320));
 		
 		Textures.creatureCardFireTruck.render(g, Wizards.compareToWidth(600), Wizards.compareToHeight(60));
 		
 		g.setColor(new Color(28, 28, 22));
 		g.drawLine(0, 300, 800, 300);
 		//Horizontal Deck Line
-		g.drawLine(200, 0, 200, 600);
+		//g.drawLine(200, 0, 200, 600);
 		//Vertical Line
 		g.drawLine(580, 0, 580, 300);
 		//Vertical Line
 		
 		g.setFont(new Font("ZapfDingbats", Font.BOLD, 31));
 		
-		g.drawString("Your Hand:", Wizards.compareToWidth(210), Wizards.compareToHeight(345));
-		g.drawString("Actions:", Wizards.compareToWidth(35), Wizards.compareToHeight(345));
-		g.drawString("Notifications:", Wizards.compareToWidth(5), Wizards.compareToHeight(40));
+		g.drawString("Your Hand:", Wizards.compareToWidth(10), Wizards.compareToHeight(345));
+		//g.drawString("Actions:", Wizards.compareToWidth(35), Wizards.compareToHeight(345));
+		//g.drawString("Notifications:", Wizards.compareToWidth(5), Wizards.compareToHeight(40));
 		g.drawString("Creatures:", Wizards.compareToWidth(210), Wizards.compareToHeight(40));
 		g.drawString("Enemies:", Wizards.compareToWidth(600), Wizards.compareToHeight(41));
-		g.drawString("HP: " + HP, Wizards.compareToWidth(212), Wizards.compareToHeight(375));
-		g.drawString("MP: " + MP, Wizards.compareToWidth(212), Wizards.compareToHeight(405));
-		g.drawString("MP Reg: " + MPRegen, Wizards.compareToWidth(210), Wizards.compareToHeight(435));
-		g.drawString("Aff: " + affinity, Wizards.compareToWidth(207), Wizards.compareToHeight(465));
+		g.drawString("HP: " + HP, Wizards.compareToWidth(12), Wizards.compareToHeight(375));
+		g.drawString("MP: " + MP, Wizards.compareToWidth(12), Wizards.compareToHeight(405));
+		g.drawString("MP Reg: " + MPRegen, Wizards.compareToWidth(10), Wizards.compareToHeight(435));
+		g.drawString("Aff: " + affinity, Wizards.compareToWidth(7), Wizards.compareToHeight(465));
 	}
 }
