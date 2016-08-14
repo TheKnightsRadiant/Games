c925620d42616b3e7a4ccc27ce75a42972b85475
ThatDude11 <ian12343456@gmail.com>
2016-08-13 21:15:41 -0600

    Cards and Players... woo

---
+++ Wizards/src/com/terracore/handlers/HordeMouseHandler.java
@@ -0,0 +1,38 @@
+package com.terracore.handlers;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class HordeMouseHandler implements MouseListener, MouseMotionListener {
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if(e.getX())
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+}
---
+++ Wizards/src/com/terracore/storages/Cards.java
@@ -0,0 +1,5 @@
+package com.terracore.storages;
+
+public class Cards {
+
+}
