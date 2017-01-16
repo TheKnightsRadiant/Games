9e63aa3bd6cbfd5aa856d5fd16dc6fa5c8a09f35
Tyler <t.eldred4646@gmail.com>
2017-01-15 23:01:29 -0700

    things

--- 4H Template/src/com/fourH/handlers/MouseHandler.java
+++ 4H Template/src/com/fourH/handlers/MouseHandler.java
@@ -4,34 +4,49 @@ import java.awt.event.*;
 
 import com.fourH.storages.*;
 import com.fourH.template.*;
+import com.fourH.storages.*;
 
-public class MouseHandler implements MouseListener, MouseMotionListener{
+public class MouseHandler implements MouseListener, MouseMotionListener {
+
+	private boolean btnAgendaHover = false;
 
 	public void mousePressed(MouseEvent e) {
-		
+
 	}
 
 	public void mouseReleased(MouseEvent e) {
-		
+
+	}
+
+	public void mouseMoved(MouseEvent e) {
+
+		if (Template.showTitleScreen = true) {
+			if (e.getX() >= Template.compareToWidth(5) && e.getX() <= Template.compareToWidth(5) + 150
+					&& e.getY() >= Template.compareToHeight(150) && e.getY() <= Template.compareToHeight(150) + 50) {
+
+				// Button Agenda
+				Textures.btnAgendaState = Textures.btnAgendaHov;
+				if (!btnAgendaHover) {
+					btnAgendaHover = true;
+				}
+			} else {
+				Textures.btnAgendaState = Textures.btnAgendaDef;
+				btnAgendaHover = false;
+			}
+
+		}
 	}
 
 	public void mouseEntered(MouseEvent e) {
-		if (Template.showTitleScreen = true){
-			
-			if (e.getX() >= Template.compareToWidth(5) && e.getX() <= Template.compareToWidth(5) + 150
-					&& e.getY() >= Template.compareToHeight(150) && e.getY() <= Template.compareToHeight(150) + 50) {
-				Textures.btnAgendaState = Textures.btnAgendaHov;
-			}
-			
-		}
 	}
-	
+
 	public void mouseExited(MouseEvent e) {
-		
 	}
-	
-	public void mouseDragged(MouseEvent e) {}
-	public void mouseMoved(MouseEvent e) {}
-	public void mouseClicked(MouseEvent e) {}
+
+	public void mouseDragged(MouseEvent e) {
+	}
+
+	public void mouseClicked(MouseEvent e) {
+	}
 
 }
